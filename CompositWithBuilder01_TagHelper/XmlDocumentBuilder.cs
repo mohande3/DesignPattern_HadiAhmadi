f0a556e204064f92e8c92ff1519b66783c309aba
mohamd taghi khademi <mohande3khademi@hotmail.com>
2023-02-03 10:30:35 +0330

    Add Method  - Add  - Add Attribure

--- CompositWithBuilder01_TagHelper/XmlDocumentBuilder.cs
+++ CompositWithBuilder01_TagHelper/XmlDocumentBuilder.cs
@@ -8,11 +8,26 @@ namespace CompositWithBuilder01_TagHelper
 {
     internal class XmlDocumentBuilder
     {
-        private readonly TagNode _rootNode;
+        private TagNode _rootNode;
+        private TagNode _currentNode;
 
         public XmlDocumentBuilder(string rootName)
         {
             _rootNode = new TagNode(rootName);
+            _currentNode = _rootNode;
+        }
+
+        public XmlDocumentBuilder AddChild(string name)
+        {
+            var tag = new TagNode(name);
+            _currentNode.Add(tag);
+            _currentNode = tag;
+            return this;
+        }
+        public XmlDocumentBuilder AddAttribute(string key,string value)
+        {
+            _rootNode.AddAttribute(key, value);
+            return this;
         }
 
         public TagNode Build()

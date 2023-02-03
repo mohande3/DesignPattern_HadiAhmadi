6e99d008f6d056e3913e9c045ec51f7724765bb0
mohamd taghi khademi <mohande3khademi@hotmail.com>
2023-02-03 07:15:42 +0330

    Add New Builder

---
+++ Builder2/Builder/HtmlReportBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace Builder2.Builder
+{
+    internal class HtmlReportBuilder : IReportBuilder
+    {
+        public void AddAuthor(string author)
+        {
+            throw new NotImplementedException();    
+        }
+
+        public void AddDescription(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Document Build()
+        {
+            return new HtmlDocument();
+        }
+    }
+}
---
+++ Builder2/Builder/IReportBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace Builder2.Builder
+{
+    internal interface IReportBuilder
+    {
+        void AddTitle(string title);    
+        void AddDescription(string description);    
+        void AddAuthor(string author);
+
+
+        Document Build();
+
+    }
+}

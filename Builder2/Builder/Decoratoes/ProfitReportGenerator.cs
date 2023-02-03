c1941eb90d079548d7178900b6125f73167f2070
mohamd taghi khademi <mohande3khademi@hotmail.com>
2023-02-03 07:18:42 +0330

    Add Decorator

---
+++ Builder2/Builder/Decoratoes/ProfitReportGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder2.Builder.Decoratoes
+{
+    internal class ProfitReportGenerator
+    {
+        private readonly IReportBuilder _reportBuiler;
+
+        public ProfitReportGenerator(IReportBuilder reportBuiler)
+        {
+            _reportBuiler = reportBuiler;
+        }
+
+        public void ReportFromVsc(string pathFile)
+        {
+            //-- Read iFile
+            _reportBuiler.AddTitle("REport from SVC");
+            _reportBuiler.AddAuthor("Mr Mohande3 Khademi");
+            _reportBuiler.AddDescription("This is a report from csv file for generation");
+        }
+    }
+}
